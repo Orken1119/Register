4e3490ac9845011e0c5859bfa79c93383509652d
Orken <ork.en389@gmail.com>
2024-11-09 15:01:09 +0500

    save22d2

--- internal/controller/router.go
+++ internal/controller/router.go
@@ -1,7 +1,10 @@
 package controller
 
 import (
+	"time"
+
 	"github.com/Orken1119/Register/pkg"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
@@ -16,6 +19,15 @@ func Setup(app pkg.Application, router *gin.Engine) {
 	loginController := &auth.AuthController{
 		UserRepository: repository.NewUserRepository(db),
 	}
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"https://your-frontend.com"}, // Укажите URL вашего фронтенда
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	router.POST("/signin-as-volunteer", loginController.SigninAsVolunteer)
