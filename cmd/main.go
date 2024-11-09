12da60c8d6af24673c48f7d2ee43de391f590d88
Orken <ork.en389@gmail.com>
2024-11-09 15:18:48 +0500

    245save22d2

--- cmd/main.go
+++ cmd/main.go
@@ -22,7 +22,7 @@ import (
 // @license.name  Apache 2.0
 // @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
 
-// @host      localhost:1140
+// @host      register-4-vyz2.onrender.com
 
 // @securityDefinitions.apikey Bearer
 // @in header
